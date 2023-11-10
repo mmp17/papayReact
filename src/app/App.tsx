c5b130abbc3735667ae27e211634143e6252fbf1
mmp17 <mirzaevmuzaffar@yahoo.com>
2023-11-11 08:34:43 +0900

    feat: develop routing based on papay project

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,50 +1,77 @@
 import React from "react"; // imports the React library, which is necessary to use React components and JSX.
 import "../css/App.css";
-import { Box, Button, Container, Stack, Typography } from "@mui/material";
 import { RippleBadge } from "./MaterialTheme/styled";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Users from "./components/users";
-import Dishes from "./components/dishes";
+
+import { RestaurantPage } from "./screens/RestaurantPage";
+import { CommunityPage } from "./screens/CommunityPage";
+import { OrdersPage } from "./screens/OrdersPage";
+import { MemberPage } from "./screens/MemberPage";
+import { HelpPage } from "./screens/HelpPagePage";
+import { LoginPage } from "./screens/LoginPage";
+import { HomePage } from "./screens/HomePage";
 
 function App() {
   // React functional component
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/dishes">Dishes</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
+        <Router>
+          <div>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/restaurant">RestaurantPage</Link>
+                </li>
+                <li>
+                  <Link to="/community">CommunityPage</Link>
+                </li>
+                <li>
+                  <Link to="/orders">OrdersPage</Link>
+                </li>
+                <li>
+                  <Link to="/member-page">MemberPage</Link>
+                </li>
+                <li>
+                  <Link to="/help">HelpPage</Link>
+                </li>
+                <li>
+                  <Link to="/login">LoginPage</Link>
+                </li>
+                <li>
+                  <Link to="/">Homepage</Link>
+                </li>
+              </ul>
+            </nav>
 
-        <Switch>
-          <Route path="/dishes">
-            <Dishes />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route path="/">
-            <Container>
-              <Home />
-            </Container>
-          </Route>
-        </Switch>
+            <Switch>
+              <Route path="/restaurant">
+                <RestaurantPage />
+              </Route>
+              <Route path="/community">
+                <CommunityPage />
+              </Route>
+              <Route path="/orders">
+                <OrdersPage />
+              </Route>
+              <Route path="/member-page">
+                <MemberPage />
+              </Route>
+              <Route path="/help">
+                <HelpPage />
+              </Route>
+              <Route path="/login">
+                <LoginPage />
+              </Route>
+              <Route path="/">
+                <HomePage />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
       </div>
     </Router>
   );
-}
-
-function Home() {
-  return <h2>Home</h2>;
 }
 
 export default App;
