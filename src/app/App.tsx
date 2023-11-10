d250a4baed9d0e85799375ed3cc4669f555779c2
mmp17 <mirzaevmuzaffar@yahoo.com>
2023-11-11 07:54:33 +0900

    feat: checking react router dom

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,10 +2,49 @@ import React from "react"; // imports the React library, which is necessary to u
 import "../css/App.css";
 import { Box, Button, Container, Stack, Typography } from "@mui/material";
 import { RippleBadge } from "./MaterialTheme/styled";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Users from "./components/users";
+import Dishes from "./components/dishes";
 
 function App() {
   // React functional component
-  return <Container></Container>;
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/dishes">Dishes</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/dishes">
+            <Dishes />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Container>
+              <Home />
+            </Container>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
 }
 
 export default App;
