8f635c363b5b9c0021016d6f819295af80ac6580
mmp17 <mirzaevmuzaffar@yahoo.com>
2023-11-12 16:57:41 +0900

    feat: develop header logic of restaurant and others navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -23,9 +23,9 @@ function App() {
       {main_path === "/" ? (
         <NavbarHome setPath={setPath} />
       ) : main_path.includes("/restaurant") ? (
-        <NavbarRestaurant />
+        <NavbarRestaurant setPath={setPath} />
       ) : (
-        <NavbarOthers />
+        <NavbarOthers setPath={setPath} />
       )}
       {/* <nav>
               <ul>
--- src/app/components/header/restaurant.tsx
+++ src/app/components/header/restaurant.tsx
@@ -1,5 +1,80 @@
 import React from "react";
+import {
+  Badge,
+  Box,
+  Button,
+  Container,
+  IconButton,
+  Stack,
+} from "@mui/material";
+import { NavLink } from "react-router-dom";
 
-export function NavbarRestaurant() {
-  return <div>NavbarRestaurant</div>;
+export function NavbarRestaurant(props: any) {
+  return (
+    <div className="format_restaurant home_navbar">
+      <Container>
+        <Stack
+          flexDirection={"row"}
+          className="navbar_config"
+          justifyContent={"space-between"}
+        >
+          <Box>
+            <img src="/icons/papay.svg" alt="papay-pic" />
+          </Box>
+          <Stack
+            flexDirection={"row"}
+            justifyContent="space-evenly"
+            alignItems={"center"}
+            className="navbar_links"
+          >
+            <Box className="hover-line" onClick={props.setPath}>
+              <NavLink to="/">Home</NavLink>
+            </Box>
+            <Box className="hover-line" onClick={props.setPath}>
+              <NavLink to="/restaurant" activeClassName="underline">
+                Restaurants
+              </NavLink>
+            </Box>
+            <Box className="hover-line" onClick={props.setPath}>
+              <NavLink to="/orders" activeClassName="underline">
+                Orders
+              </NavLink>
+            </Box>
+            <Box className="hover-line" onClick={props.setPath}>
+              <NavLink to="/community" activeClassName="underline">
+                Community
+              </NavLink>
+            </Box>
+            <Box className="hover-line" onClick={props.setPath}>
+              <NavLink to="/help" activeClassName="underline">
+                Help
+              </NavLink>
+            </Box>
+            <Box className="hover-line">
+              <IconButton
+                aria-label="cart"
+                id="basic-button"
+                aria-controls={undefined}
+                aria-haspopup="true"
+                aria-expanded={undefined}
+              >
+                <Badge badgeContent={3} color="secondary">
+                  <img src="/icons/shopping_cart.svg" />
+                </Badge>
+              </IconButton>
+            </Box>
+            <Box>
+              <Button
+                variant="contained"
+                style={{ color: "#FFFFFF", background: "#1976D2" }}
+              >
+                LOGIN
+              </Button>
+            </Box>
+          </Stack>
+        </Stack>
+      </Container>
+      ;
+    </div>
+  );
 }
