85c895460382385393cd78113a42b08025957421
mmp17 <mirzaevmuzaffar@yahoo.com>
2023-11-18 18:25:01 +0900

    feat: restaurant screen page all restaurant component

--- src/app/screens/RestaurantPage/allRestaurants.tsx
+++ src/app/screens/RestaurantPage/allRestaurants.tsx
@@ -1,5 +1,191 @@
 import React from "react";
+import {
+  Box,
+  Button,
+  Container,
+  Pagination,
+  PaginationItem,
+  Stack,
+} from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import {
+  AspectRatio,
+  Card,
+  CardOverflow,
+  CssVarsProvider,
+  IconButton,
+  Link,
+  Typography,
+} from "@mui/joy";
+import Favorite from "@mui/icons-material/Favorite";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import CallIcon from "@mui/icons-material/Call";
+import LocationOnRoundedIcon from "@mui/icons-material/LocationOnRounded";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+
+const order_list = Array.from(Array(8).keys());
 
 export function AllRestaurants() {
-  return <div>AllRestaurants</div>;
+  return (
+    <div className="all_restaurant">
+      <Container>
+        <Stack>
+          <Box
+            className={"fill_search_box"}
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Box className={"fill_box"}>
+              <a>Best</a>
+              <a>Famous</a>
+              <a>Trending</a>
+              <a>New</a>
+            </Box>
+            <Box className={"search_big_box"}>
+              <form className={"search_form"} action={""} method={""}>
+                <input
+                  type={"search"}
+                  className={"searchInput"}
+                  name={"resSearch"}
+                  placeholder={"Search"}
+                />
+                <Button
+                  className={"button_search"}
+                  variant="contained"
+                  endIcon={<SearchIcon />}
+                >
+                  {/* Search */}
+                </Button>
+              </form>
+            </Box>
+          </Box>
+          <Stack className={"all_res_box"}>
+            <CssVarsProvider>
+              {order_list.map((ele) => {
+                return (
+                  <Card
+                    variant="outlined"
+                    sx={{
+                      minHeight: 410,
+                      minWidth: 290,
+                      mx: "17px",
+                      my: "20px",
+                    }}
+                  >
+                    <CardOverflow>
+                      <AspectRatio ratio="1">
+                        <img src={"/restaurant/burak.jpeg"} />
+                      </AspectRatio>
+                      <IconButton
+                        aria-label="Like minimal photography"
+                        size="md"
+                        variant="solid"
+                        color="danger"
+                        sx={{
+                          position: "absolute",
+                          zIndex: 2,
+                          borderRadius: "50%",
+                          right: "1rem",
+                          bottom: 0,
+                          transform: "translateY(50%)",
+                          color: "rgba(0,0,0,.4)",
+                        }}
+                      >
+                        <Favorite /* @ts-ignore */ style={{ color: "white" }} />
+                      </IconButton>
+                    </CardOverflow>
+                    <Typography level="h2" sx={{ fontSize: "md", mt: 2 }}>
+                      Texas De Brazil
+                    </Typography>
+                    <Typography level="body-sm" sx={{ mt: 0.5, mb: 2 }}>
+                      <Link
+                        href=""
+                        startDecorator={<LocationOnRoundedIcon />}
+                        textColor="neutral.700"
+                      >
+                        Tashkent Yunusabad
+                      </Link>
+                    </Typography>
+                    <Typography level="body-sm" sx={{ mt: 0.5, mb: 2 }}>
+                      <Link
+                        href=""
+                        startDecorator={<CallIcon />}
+                        textColor="neutral.700"
+                      >
+                        998902669955
+                      </Link>
+                    </Typography>
+                    <CardOverflow
+                      sx={{
+                        display: "flex",
+                        flexDirection: "row",
+                        gap: 1.5,
+                        py: 1.5,
+                        // px: "var(--Card--padding",
+                        borderTop: "1px solid",
+                        borderColor: "neutral.outlineBorder",
+                        bgcolor: "background.level1",
+                      }}
+                    >
+                      <Typography
+                        level="body-md"
+                        sx={{
+                          fontweight: "md",
+                          color: "text.secondary",
+                          alignItems: "center",
+                          display: "flex",
+                        }}
+                      >
+                        100{""}
+                        <VisibilityIcon
+                          sx={{ fontSize: 20, marginLeft: "5px" }}
+                        />
+                      </Typography>
+                      <Box sx={{ width: 2, bgcolor: "divider" }} />
+                      <Typography
+                        level="body-md"
+                        sx={{
+                          fontweight: "md",
+                          color: "text.secondary",
+                          alignItems: "center",
+                          display: "flex",
+                        }}
+                      >
+                        <div>50</div>
+                        <Favorite sx={{ fontSize: 20, marginLeft: "5px" }} />
+                      </Typography>
+                    </CardOverflow>
+                  </Card>
+                );
+              })}
+            </CssVarsProvider>
+          </Stack>
+
+          <Stack className={"bottom_box"}>
+            <img className={"line_img"} src={"/restaurant/line.svg"} />
+            <Pagination
+              count={3}
+              page={1}
+              renderItem={(item) => (
+                <PaginationItem
+                  components={{
+                    previous: ArrowBackIcon,
+                    next: ArrowForwardIcon,
+                  }}
+                  {...item}
+                  color={"secondary"}
+                />
+              )}
+            />
+            <img className={"line_img_two"} src={"/restaurant/line_two.svg"} />
+          </Stack>
+        </Stack>
+      </Container>
+    </div>
+  );
 }
