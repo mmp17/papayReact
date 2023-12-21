849e3c571eb3514e192af937e7e100a553c96a8e
mmp17 <mirzaevmuzaffar@yahoo.com>
2023-12-21 16:03:07 +0500

    fix: usage of slice and selectors on homePage screen components

--- src/app/screens/HomePage/index.tsx
+++ src/app/screens/HomePage/index.tsx
@@ -9,12 +9,39 @@ import { Events } from "./events";
 import { Recommendations } from "./recommendations";
 import "../../../css/home.css";
 
+//Redux
+import { useDispatch, useSelector } from "react-redux";
+import { Dispatch } from "@reduxjs/toolkit";
+import { createSelector } from "reselect";
+import { setTopRestaurants } from "../../screens/HomePage/slice";
+import { retrieveTopRestaurants } from "../../screens/HomePage/selector";
+import { Restaurant } from "../../../types/user";
+
+//** Redux Slice */
+const actionDispatch = (dispatch: Dispatch) => ({
+  setTopRestaurants: (data: Restaurant[]) => dispatch(setTopRestaurants(data)),
+});
+
+//** Redux Selector */
+const topRestaurantsRetriever = createSelector(
+  retrieveTopRestaurants,
+  (topRestaurants) => ({
+    topRestaurants,
+  })
+);
+
 export function HomePage() {
+  // selector: store => data
+  // Initialization
+  const { setTopRestaurants } = actionDispatch(useDispatch());
+  const { topRestaurants } = useSelector(topRestaurantsRetriever);
+
+  console.log("topRestaurants:::", topRestaurants);
+
   useEffect(() => {
-    console.log("componentDidMount => Data fetch");
-    return () => {
-      console.log("componentWillUnmount process");
-    };
+    // backend data request => data
+    // slice: data => store
+    setTopRestaurants([]);
   }, []);
   return (
     <div className="homepage">
